3b91611519caa32975db70c0bf0492640ee12675
SwapneshSahu <114054368+SwapneshSahu@users.noreply.github.com>
2024-09-12 17:28:40 +0530

    Initial Commit

---
+++ src/test/java/com/Assessment/TDD_DEMO/TddDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Assessment.TDD_DEMO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
